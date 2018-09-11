59a22302c784aa648707d3e76ca69d1cae75b317
Tobias Ebert <tobias.ebert@mbi.de>
2018-09-11 23:53:19 +0200

    Fix names in Windows demo

--- nodejs/windows/docker-start.bat
+++ nodejs/windows/docker-start.bat
@@ -1,1 +1,1 @@
-docker -H giessen.ghana.mbi.de run --rm -d -p 8080:8080 --name nodeexample nodeexample
+docker -H giessen.ghana.mbi.de run --rm -d -p 8080:8080 --name nodeexamplewin nodeexamplewin
--- nodejs/windows/docker-stop.bat
+++ nodejs/windows/docker-stop.bat
@@ -1,2 +1,2 @@
-docker -H giessen.ghana.mbi.de stop nodeexample
-docker -H giessen.ghana.mbi.de rm nodeexample
+docker -H giessen.ghana.mbi.de stop nodeexamplewin
+docker -H giessen.ghana.mbi.de rm nodeexamplewin
